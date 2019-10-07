59dc537a1d47b6bb70dfda8319ae553be99661cf
Tatyaniya <tatyaniya@bigmir.net>
2019-10-07 21:17:25 +0300

    home

---
+++ smartgrid.js
@@ -0,0 +1,24 @@
+module.exports = {
+	columns: 12,
+	offset: "10px",
+	container: {
+		maxWidth: "1200px",
+		fields: "30px"
+	},
+	breakPoints: {
+		md: {
+			width: "992px",
+			fields: "20px"
+		},
+	   sm: {
+	      width: "764px"
+	   },
+	   xs: {
+	      width: "576px"
+	   },
+	   xxs: {
+	      width: "400px"
+	   }
+	},
+	mobileFirst: false
+};
